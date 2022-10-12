234da3dab91a05764a4b602410bd6022babf5387
Mohamed Tarek <58186533+M0hamed2022@users.noreply.github.com>
2022-10-12 02:45:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let Eldeep = document.querySelector('.Eldeep');
+window.onscroll = function(){
+    let value = scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top = value * 4 + 'px';
+        mountains3.style.top = value * 2 + 'px';
+        mountains4.style.top = value * 1.5 + 'px';
+        river.style.top = value + 'px';
+        boat.style.top = value + 'px';
+        boat.style.left = value * 3 + 'px';
+        Eldeep.style.fontSize = value  + 'px';
+        if(scrollY >= 102){
+            Eldeep.style.fontSize = 102  + 'px';
+            Eldeep.style.position = 'fixed';
+            if(scrollY >= 530){
+                Eldeep.style.display = 'none';
+            }else{
+                Eldeep.style.display = 'block';
+            }
+            if(scrollY >= 150){
+                document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)'
+            }else{
+                document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+            }
+        }
+   }
